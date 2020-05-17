d754bc8ce1ff69cb81ba9fccc8880022877d50d3
NotStatilko <valerastatilko@gmail.com>
2020-05-18 02:08:09 +0300

    Update all files to UNSTABLE tgback3.0

--- TelegramBackup.py
+++ TelegramBackup.py
@@ -1,44 +1,42 @@
-print('\n' * 100 + '@ TelegramBackup is loading...')
+print('\n' * 35 + '@ TelegramBackup is loading...')
 
 import os.path
 
-from base64 import b64encode
-from NonCipher import NonCipher
+from time import ctime
+from getpass import getpass
+from traceback import print_exc
 from os import system as os_system_
 from asyncio import run as asyncio_run
 from telethon.errors import FloodWaitError
+from telethon.errors.rpcbaseerrors import AuthKeyError
 
-from tgback_utils import (
-    TelegramAccount, restore, make_session
-)
+from tgback_utils import TelegramAccount, restore
 
 def clear_terminal():
-    os_system_('cls || clear')
-    print('\n' * 50)
-
+    os_system_('cls || clear'); print('\n' * 35)
+    
 
 async def main():
     try:
         clear_terminal()
-        while True:
-            print(''' - TelegramBackup 2.0 -\n\n'''
-                  '''> 1) Backup Telegram account\n'''
-                  '''>> 2) Change phone number\n'''
-                  '''>>> 3) Decrypt .tgback backup\n'''
-                  '''>>>> 4) Exit from TelegramBackup'''
-                  '''\n\n% Press Ctrl+C in any mode for returning to this page'''
+        while True:                   
+            print(
+                ''' - TelegramBackup 3.0 -\n\n'''
+                '''> 1) Backup Telegram account\n'''
+                '''>> 2) Open .etgback backup\n'''
+                '''>>> 3) Exit from TelegramBackup'''
+                '''\n\n% Press Ctrl+C in any mode for returning to this page'''
             )
             selected_section = input('\n@ Input: ')
-            if selected_section in list('1234') + ['what\'s new?']:
-                break
-            clear_terminal()
+            if selected_section in '123':
+                break                      
 
         while True:
             clear_terminal()
             if selected_section == '1':
-                api_id = input('> API ID: ')
+                api_id = getpass('> API ID (hidden input): ')
                 clear_terminal()
-                api_hash = input('>> API Hash: ')
+                api_hash = getpass('>> API Hash: ')
                 clear_terminal()
                 phone = input('>>> Phone number: ')
                 try:
@@ -54,7 +52,7 @@ async def main():
                     clear_terminal()
 
                     code = input('> Confirmation Code: ')
-                    password = input('>> Your Telegram password: ')
+                    password = getpass('>> Your Telegram password: ')
                     clear_terminal()
 
                     print('@ Trying to login...')
@@ -63,126 +61,125 @@ async def main():
                         clear_terminal()
 
                         tgback_filename = input('> Backup filename: ')
-                        tgback_password = input('>> Backup password: ')
+                        tgback_password = getpass('>> Backup password: ')
 
                         clear_terminal()
                         print('@ Backup password generating, please wait...')
 
-                        file = account.backup(tgback_password, tgback_filename)
+                        filename = await account.backup(tgback_password, tgback_filename)
                         clear_terminal()
-
-                        account.remove_session()
-                        input('@ Successfully backuped and encrypted! ({0})'.format(file))
+                        
+                        input(f'@ Successfully backuped and encrypted! ({filename})')
                         await main()
 
                     except (KeyboardInterrupt, EOFError):
                         await main()
                     except:
-                        input('\n@: ! Invalid password or code. Try again.')
+                        input('\n@: ! Incorrect password or code. Try again.')
 
                 except FloodWaitError as e:
                     clear_terminal()
                     input('''@: ! Telegram servers return FloodWaitError. '''
-                        '''Please wait {0} seconds '''.format(e.seconds))
+                        f'''Please wait {e.seconds} seconds ''')
                     await main()
                 except:
                     clear_terminal()
                     input('''@: ! App Invalid! Copy and paste from the '''
                           '''site my.telegram.org. Help: bit.ly/tgback '''
-                    )
-                finally:
-                    account.remove_session()
-
+                    )                
             elif selected_section == '2':
                 clear_terminal()
-                path_to_tgback = input('> Path to .tgback file: ')
+                path_to_tgback = input('> Path to .etgback file: ')
                 if not os.path.exists(path_to_tgback):
-                    input('@: ! Can\'t open .tgback file. Check your path. ')
+                    input('@: ! Can\'t open .etgback file. Check your path. ')
                     await main()
                 else:
-                    tgback_password = input('>> Password to .tgback file: ')
+                    tgback_password = getpass('>> Password to .etgback file: ')
                     clear_terminal()
-                    print('@ Backup password generating, please wait...')
+                    print('@ Password for backup is generating, please wait...')
                     try:
                         restored = restore(path_to_tgback, tgback_password)
-                        restored_backup = restored[0]
-                        primary_hash, hash_of_input_data = restored[1]
-                    except:
-                        clear_terminal()
+                        assert len(restored) == 8
+                    except:                        
+                        clear_terminal()                       
                         input('\n@: ! Incorrect Password. ')
                         await main()
                     else:
-                        clear_terminal()
-                        make_session(restored_backup)
-                        account = TelegramAccount(*restored_backup[1], restored_backup[0])
+                        account = TelegramAccount(
+                            *restored[:2], session=restored[3]
+                        )
                         await account.connect()
                         while True:
                             clear_terminal()
-                            new_phone = input('> Enter your new phone number: ')
-                            try:
-                               code_hash = await account.request_change_phone_code(new_phone)
-                            except:
-                                input('\n@: ! Number is invalid or already in use. ')
-                            else: break
-
-                        while True:
-                            clear_terminal()
-                            print('>> Confirmation code has been sent to ' + new_phone)
-                            code = input('@ >> Code: ')
-
-                            try:
-                                await account.change_phone(code, code_hash, new_phone)
-                            except:
-                                input('\n@: ! Invalid code. Try again or hit Ctrl+C. ')
-                            else: break
-
-                        clear_terminal()
-                        account.remove_session()
-
-                        new_backup = (new_phone, restored_backup[1], restored_backup[2])
-                        nc = NonCipher(primary_hash=primary_hash, hash_of_input_data=hash_of_input_data)
-                        nc.init()
-
-                        with open(path_to_tgback,'wb') as f:
-                            f.write(b64encode(nc.encrypt(repr(new_backup)).encode()))
-
-                        input('''@: Your phone has been successfully changed! '''
-                            '''Now you can login to your Telegram account with phone ''' + new_phone + ' ! ')
-                        await main()
-
+                            return_to_page = False
+                            print(
+                                f'''% Hello, {restored[5] + restored[7]}! (id{restored[6]})\n\n'''
+                                f'''@ Backup valid until {ctime(float(restored[4]))}\n'''
+                                f'''@ API ID / API Hash: {restored[0][:4]}... / {restored[1][:4]}...\n\n'''                                                                                               
+                                f'''> 1) Change account phone number\n'''
+                                f'''>> 2) Refresh .etgback backup\n'''
+                                f'''>>> 3) Return to main page'''
+                            )                            
+                            selected_section = input('\n@ Input: ')                                                                                                                                   
+                            if selected_section == '1':                  
+                                clear_terminal()                                                              
+                                while True:
+                                    clear_terminal()
+                                    new_phone = input('> Enter your new phone number: ')
+                                    try:
+                                       code_hash = await account.request_change_phone_code(new_phone)
+                                    except AuthKeyError:
+                                        return_to_page = True
+                                        input('\n@: ! Can\'t change phone number now. Please, wait some time.')
+                                        break                                        
+                                    except:                                       
+                                        input('\n@: ! Number is invalid or already in use. ')
+                                    else:
+                                        break
+                                
+                                if not return_to_page:
+                                    while True:                                    
+                                        clear_terminal()
+                                        print('>> Confirmation code has been sent to ' + new_phone)
+                                        code = input('@ >> Code: ')
+                                        try:
+                                            await account.change_phone(code, code_hash, new_phone)
+                                        except:
+                                            input('\n@: ! Invalid code. Try again or hit Ctrl+C. ')
+                                        else: break
+            
+                                    clear_terminal()                                          
+                                    input('''@: Your phone has been successfully changed! '''
+                                        f'''Now you can login to your Telegram account with phone {new_phone}!''')
+                                    await main()
+                            
+                            elif selected_section == '2':
+                                await account.refresh_backup(restored, path_to_tgback)
+                                clear_terminal()
+                                input('@ Successfully refreshed! | ')
+                                
+                            elif selected_section == '3':
+                                await main()
+                                
             elif selected_section == '3':
-                path_to_backup = input('> Direct path to .tgback file: ')
-                if os.path.exists(path_to_backup):
-                    tgback_password = input('>> Password for .tgback file: ')
-
-                    clear_terminal()
-                    print('@ Backup password generating, please wait...')
-
-                    name = os.path.split(path_to_backup)[1].split('.tgback')[0]
-
-                    try:
-                        restored = restore(path_to_backup, tgback_password)
-                        clear_terminal()
-                    except:
-                        input('\n@: ! Incorrect Password. ')
-                        await main()
-                    else:
-                        with open(name + '.dtgback','wb') as f:
-                            f.write(repr(restored).encode())
-
-                        input('@ Successfully decrypted! ({0})'.format(name + '.dtgback'))
-                        await main()
-                else:
-                    input('\n@: ! Can\'t open .tgback file. Please check your path.')
-
-            elif selected_section == '4':
-                exit()
-
-            elif selected_section == 'what\'s new?':
-                input('@: # New mode and first stable version! bit.ly/tgback. NonSense. ')
+                raise SystemExit
+            else:
                 await main()
-
-    except (KeyboardInterrupt, EOFError):
+            
+    except (KeyboardInterrupt, EOFError): await main()        
+    except SystemExit: raise SystemExit    
+    except Exception as e:
+        clear_terminal()
+        print_exc(file=open('tgback.log','a'))       
+        print(
+            f'''@: ! Oops, something went wrong! Unknown error was '''
+            '''written to the "tgback.log" file, so please, '''
+            '''send it to me on Telegram (t.me/not_statilko), '''
+            '''or open issue on GitHub (bit.ly/tgback). '''
+            '''I will fix it as soon as possible. Thanks in advance!\n\n'''
+            f'''Short error: {e}\n'''
+        )
+        input('@ Press Enter to return |')
         await main()
 
 
--- tgback_utils.py
+++ tgback_utils.py
@@ -1,110 +1,202 @@
-from sqlite3 import connect as sqlite3_connect
-from base64 import b64encode, b64decode
-from hashlib import md5, sha512
-from shutil import copyfile
-from ast import literal_eval
-from time import time
+from time import time, ctime
+from datetime import timedelta
 
 from telethon import TelegramClient
 from telethon.errors import PhoneNumberInvalidError, SessionPasswordNeededError
 from telethon.tl.functions.account import ChangePhoneRequest, SendChangePhoneCodeRequest
 from telethon.tl.types import CodeSettings
-from telethon.sessions.abstract import Session
+from telethon.sessions import StringSession
 
-from NonCipher import NonCipher, get_hash_of
+from os.path import split as path_split
+from hashlib import md5, sha1, sha512, sha3_256
+from pyaes import AESModeOfOperationCBC, Encrypter, Decrypter
+from pyaes.util import append_PKCS7_padding, strip_PKCS7_padding
+
+
+class TgbackAES:
+    def __init__(self, password: bytes):        
+        if not isinstance(password, bytes):
+            password = password.encode()
+            
+        self.__raw_password = password        
+        self.__password_hash = None; self.__iv = None
+    
+    @staticmethod
+    def _hash_password(password: bytes, iter_count=2_222_222) -> sha3_256:        
+        init = sha512(sha1(md5(password).digest()).digest())
+        init = sha512(sha3_256(md5(init.digest()).digest()).digest())
+        
+        for _ in range(iter_count):
+            init = sha3_256(init.digest())
+        
+        return init
+    
+    def init(self, password_hash: bytes=None) -> None:               
+        if password_hash:
+            self._password_hash = password_hash
+        else:
+            if not self.__raw_password:
+                raise Exception('Already initialized')
+            else:                
+                self._password_hash = self._hash_password(self.__raw_password).digest()                  
+                self.__raw_password = None
+        
+        self.__iv = sha512(self._password_hash).digest()[:16] 
+        
+    def encrypt(self, data: bytes) -> bytes:
+        if not self._password_hash:
+            raise Exception('You need to call .init() for first')
+        else:            
+            if not isinstance(data, bytes):
+                data = data.encode()
+                
+            if len(data) % 16:
+                data = append_PKCS7_padding(data)
+            
+            aes_cbc = Encrypter(
+                AESModeOfOperationCBC(
+                    self._password_hash, self.__iv)
+            )
+            encrypted = aes_cbc.feed(data)
+            encrypted += aes_cbc.feed()           
+            return encrypted
+    
+    def decrypt(self, edata: bytes) -> bytes:
+        if not self._password_hash:
+            raise Exception('You need to call .init() for first')
+                        
+        elif not isinstance(edata, bytes):
+            raise Exception('edata must be bytes')            
+        else:
+            aes_cbc = Decrypter(
+                AESModeOfOperationCBC(
+                    self._password_hash, self.__iv)
+            )
+            decrypted = aes_cbc.feed(edata)
+            decrypted += aes_cbc.feed()
+            return strip_PKCS7_padding(decrypted)
 
 
 class TelegramAccount:
-    def __init__(self, api_id: int, api_hash: str, phone_number: str):
-        self._api_id = api_id
+    def __init__(self, api_id: int, api_hash: str, phone_number: str=None, session: str=None):
+        self._api_id = int(api_id)
         self._api_hash = api_hash
-        self._phone_number = phone_number
-        self._session_filename = 'tgback_' + md5(str(api_id).encode()).hexdigest() + '.session'
+        self._phone_number = phone_number       
         self._TelegramClient = TelegramClient(
-            self._session_filename,
-            self._api_id, self._api_hash
+            StringSession(session), self._api_id, self._api_hash
         )
-    async def connect(self):
+        self.__notify = (
+            """Your {5}backup has been {0}!\n\nRemember that """
+            """**every two months** it needs to be updated, otherwise you will """
+            """**lose access** to `{1}`. I created a delayed message for """
+            """you a week before the time expires, you will be automatically """
+            """notified and will have time to update.\n\n"""
+            """`@ {2}:  {3}`\n`@ Will off: {4}`\n\nStay Cool!"""        
+        )
+    @staticmethod
+    def __prepare_user_entity(entity):
+        if not entity.username:
+            entity.username  = entity.first_name
+            entity.last_name = '' if not entity.last_name else entity.last_name
+        else:
+            entity.username  = '@' + entity.username
+            entity.last_name = ''
+        
+        return entity
+    
+    async def connect(self) -> None:
         await self._TelegramClient.connect()
-
-    def remove_session(self):
-        try:
-            self._TelegramClient.session._conn.commit()
-            self._TelegramClient.session.close()
-            self._TelegramClient.session.delete()
-        except:
-            return False
-        return True
-
-    async def request_code(self):
+    
+    async def request_code(self) -> None:
         await self._TelegramClient.send_code_request(self._phone_number)
 
-    async def login(self, password: str=None, code: int=None):
+    async def login(self, password: str=None, code: int=None) -> None:
         if not await self._TelegramClient.is_user_authorized():
             try:
                 await self._TelegramClient.sign_in(self._phone_number, code)
             except SessionPasswordNeededError:
                 await self._TelegramClient.sign_in(password=password)
 
-    async def request_change_phone_code(self, new_number: str):
-        request = change_phone_request = await self._TelegramClient(SendChangePhoneCodeRequest(
+    async def request_change_phone_code(self, new_number: str) -> str:
+        request = await self._TelegramClient(SendChangePhoneCodeRequest(
             phone_number = new_number,
             settings = CodeSettings(allow_flashcall=True, current_number=True)
         ))
         return request.phone_code_hash
 
-    async def change_phone(self, code: str, code_hash: str, new_number: str):
+    async def change_phone(self, code: str, code_hash: str, new_number: str) -> None:
         await self._TelegramClient(ChangePhoneRequest(
             phone_number=new_number, phone_code=code,
             phone_code_hash=code_hash
         ))
+        
+    async def backup(self, password: bytes, filename: str=None) -> str:
+        tgback_aes = TgbackAES(password)
+        tgback_aes.init()
 
-    def backup(self, password: str , filename: str=None):
-        nc = NonCipher(password, sha512(password.encode()).hexdigest(), 5_000_000)
-        nc.init()
-
-        filename = (filename if filename else str(int(time()))) + '.tgback'
-
-        session_db = sqlite3_connect(self._session_filename)
-        session_cursor = session_db.cursor()
-
-        metadata = session_cursor.execute('SELECT * FROM sessions')
-        metadata = metadata.fetchall()
-        session_db.close()
-
-        to_backup = (self._phone_number,
-            (self._api_id, self._api_hash), metadata[0]
+        filename = (filename if filename else str(int(time()))) + '.etgback'        
+        
+        user = await self._TelegramClient.get_entity('me')        
+        user = self.__prepare_user_entity(user)
+        
+        current_time = time()
+        backup_death_at = current_time + 5_356_800
+        
+        backup_data =  f'{self._api_id}<*>{self._api_hash}<*>'
+        backup_data += f'{tgback_aes._password_hash.hex()}<*>'
+        backup_data += self._TelegramClient.session.save()
+        backup_data += f'<*>{backup_death_at}<*>{user.username}<*>{user.id}'
+        backup_data += f'<*>{user.last_name}'
+                                                      
+        await self._TelegramClient.send_message('me',self.__notify.format(
+            'created', filename, 'Created', ctime(), ctime(backup_death_at),'new ')
         )
-        backup = b64encode(nc.encrypt(repr(to_backup)).encode())
-
-        with open(filename,'wb') as f:
-            f.write(backup)
+        await self._TelegramClient.send_message('me',
+            f'Hello! Please, update your backup `{filename}`. \n\n**One week left!!**',
+            schedule=timedelta(seconds=4_752_000)
+        )
+        with open(filename,'w') as f:
+            f.write(tgback_aes.encrypt(backup_data).hex())
 
         return filename
+    
+    async def refresh_backup(self, restored: list, tgback_file_path: str) -> None:        
+        backup_death_at = time() + 5_356_800
+        
+        user = await self._TelegramClient.get_entity('me')
+        user = self.__prepare_user_entity(user)
+        
+        restored[4] = backup_death_at
+        restored[5] = user.username
+        restored[6] = user.id
+        restored[7] = user.last_name
+        
+        dump(restored, tgback_file_path)
+        
+        await self._TelegramClient.send_message('me',self.__notify.format(
+            'updated', tgback_file_path, 'Updated', 
+            ctime(), ctime(backup_death_at),'')
+        )
+        backup_name = path_split(tgback_file_path)[-1]
+        await self._TelegramClient.send_message('me',
+            f'Hello! Please, update your backup `{backup_name}`\n\n**One week left!!**',
+            schedule=timedelta(seconds=4_752_000)
+        )
+
+def restore(tgback_file_path: str, password: bytes) -> list:
+    tgback_aes = TgbackAES(password)
+    tgback_aes.init()
+        
+    with open(tgback_file_path,'rt') as f:       
+        return tgback_aes.decrypt(
+            bytes(bytearray.fromhex(f.read()))
+        ).decode().split('<*>')
 
 
-def restore(tgback_file_path: str, password: str):
-    nc = NonCipher(password, sha512(password.encode()).hexdigest(), 5_000_000)
-    nc.init()
-
-    with open(tgback_file_path,'rb') as f:
-        restored = nc.decrypt(b64decode(f.read()))
-        return (literal_eval(restored.decode()), (nc._primary_hash, nc._hash_of_input_data))
-
-
-def make_session(restored_backup: tuple):
-    session_filename = 'tgback_' + md5(
-        str(restored_backup[1][0]).encode()
-    ).hexdigest() + '.session'
-
-    copyfile('template.tsession', session_filename)
-
-    session_db = sqlite3_connect(session_filename)
-    session_cursor = session_db.cursor()
-
-    session_cursor.execute(
-        'INSERT INTO sessions VALUES (?,?,?,?,?)',
-        restored_backup[2]
-    )
-    session_db.commit()
-    session_db.close()
+def dump(restored: list, tgback_file_path: str):    
+    tgback_aes = TgbackAES('')
+    tgback_aes.init(bytearray.fromhex(restored[2]))   
+       
+    with open(tgback_file_path,'w') as f:
+        restored_str = (str(i) for i in restored)
+        f.write(tgback_aes.encrypt('<*>'.join(restored_str)).hex())
