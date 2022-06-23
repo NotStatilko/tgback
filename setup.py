adf6426c35d79b66341fd8adadc2d889ea8e187b
NotStatilko <valerastatilko@gmail.com>
2022-06-23 09:12:41 +0300

    Update setup.py to prepare for PIP release

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name = "tgback",
     version = '5.0',
     py_modules = ['app','tools'],
+    license = 'MIT',
+    description = 'tgback — a program for backing and restoring Telegram accounts',
+    author_email = 'thenonproton@pm.me',
+    url = 'https://github.com/NotStatilko/tgback',
+    download_url = 'https://github.com/NotStatilko/tgback/archive/refs/tags/v5.0.tar.gz',
     install_requires = [
         'pyaes==1.6.1',
         'telethon==1.24.0'
